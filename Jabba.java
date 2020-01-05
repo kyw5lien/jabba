31dc8fc8fb74b7d73bea5663d9585b63ef22e9ad
kyw5lien <cout.neves@gmail.com>
2020-01-05 22:17:14 -0100

    Jabba.java, a classic SW interpretation of a HelloSith Java program.
    // Jabba.java, a classic SW interpretation of a HelloSith Java program.

---
+++ Jabba.java
@@ -0,0 +1,15 @@
+/**
+ * 	Jabba Class: a SW interpretation of the classic HelloSith Java program.
+ */
+
+// package jabbaPackage;
+
+/**
+ * @author kyw5lien
+ * 
+ */
+public class Jabba {
+	public static void main(String[] args) {
+		System.out.println("Hello Jabba! Bring me solo.");
+	}
+}
